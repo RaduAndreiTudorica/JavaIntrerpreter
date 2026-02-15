ed2316e8012bfc0305cde83f3c9ce50d675e7f22
Tudorica Radu Andrei <raduandreitudorica3@gmail.com>
2026-02-16 01:19:00 +0200

    Uninitialized variables error

--- src/lox/Environment.java
+++ src/lox/Environment.java
@@ -6,40 +6,46 @@ import java.util.Map;
 public class Environment {
     final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
-
+    private static final Object UNINITIALIZED = new Object();
+    
     Environment() {
         enclosing = null;
     }
-
+    
     Environment(Environment enclosing) {
         this.enclosing = enclosing;
     }
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
-            return values.get(name.lexeme);
-        }
+            Object value = values.get(name.lexeme);
+            if (value == UNINITIALIZED) {
+                throw new RuntimeError(name, "Variable '" + name.lexeme + "' is not initialized.");
+            }
 
+            return value;
+        }
+        
         if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
     void define(String name, Object value) {
-        values.put(name, value);
+        values.put(name, value == null ? UNINITIALIZED : value);
     }
-
+    
     void assign(Token name, Object value) {
-        if (values.containsKey(name.lexeme)) {
+        if(values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
             return;
         }
-
+        
         if (enclosing != null) {
             enclosing.assign(name, value);
             return;
         }
-
+        
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 }
